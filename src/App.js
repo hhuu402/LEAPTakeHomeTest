f15df587db69cdd1612f7c5951d7857400ebcd54
hhuu402 <47773127+hhuu402@users.noreply.github.com>
2022-09-07 12:43:24 +1000

    Add files via upload
    changed db

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ import Display from './components/pages/Display';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBNY5PixK947f9BMqNiAmiVYliMOolsO0k",
+  authDomain: "leap-project-d3f7c.firebaseapp.com",
+  projectId: "leap-project-d3f7c",
+  storageBucket: "leap-project-d3f7c.appspot.com",
+  messagingSenderId: "916591176978",
+  appId: "1:916591176978:web:0d11c8d61fc36285ba97cb",
+  measurementId: "G-SKG8GWB5H9"
+};
+
 function App() {
   return (
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkNv__zpf1r2nN0PWz4JB_4n8KvRQJFX4",
-    authDomain: "leap-activities.firebaseapp.com",
-    projectId: "leap-activities",
-    storageBucket: "leap-activities.appspot.com",
-    messagingSenderId: "835952134921",
-    appId: "1:835952134921:web:ebd3ffd9bf812eed5521d8",
-    measurementId: "G-1J5HEX8WRJ"
+    apiKey: "AIzaSyBNY5PixK947f9BMqNiAmiVYliMOolsO0k",
+    authDomain: "leap-project-d3f7c.firebaseapp.com",
+    projectId: "leap-project-d3f7c",
+    storageBucket: "leap-project-d3f7c.appspot.com",
+    messagingSenderId: "916591176978",
+    appId: "1:916591176978:web:0d11c8d61fc36285ba97cb",
+    measurementId: "G-SKG8GWB5H9"
 };
 
 const app = initializeApp(firebaseConfig);
